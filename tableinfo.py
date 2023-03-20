c94ec3186b13941ae8b52b7444644b39de502bc1
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-03-20 15:22:01 +0800

    First commit

---
+++ tableinfo.py
@@ -0,0 +1,93 @@
+ food = [
+  "Cheesy-O-Burger Meal","Hotouch Burger Meal","Lucky Plate Meal",
+  "Chick-A-Licious with Black pepper Sauce","Chick-A-Licious with Mushroom Sauce","Double Chicken Delight:","Fish N Chips",
+"Meals","JomMakan Nasi Kandar","JomMakan Nasi Ayam","BuburAyam Meal","BuburPedasMeal","Nasi Kari Kapitan","Fish Fillet Burger Meal","Tower Burger Meal"]
+
+drinks=["Coca-Cola","FantaStrawberry","FantaGrape","Minute Maid Fresh Orange","Heaven and Earth",
+"Iced Lemon","NescafeTarik","TeaTarik","Milo","NescafeIce","TeaIce","Milo Ice"]
+
+statuses =[
+  "WAIT FOR ORDER","ORDERED","IN PROGRESS","READY TO SERVE", "SERVED", "HOUSEKEEPING","AVAILABLE"
+]
+
+
+tableinfo={
+"A":{
+"drinks":{
+"itemdrink":[""],
+"qdrink":[""]
+},
+"food": {
+"itemfood":[""],
+"qfood":[""]
+} ,
+"status": "AVAILABLE",
+"table": "A",
+"time": ""
+},
+"B":{
+"drinks":{
+"itemdrink":[""],
+"qdrink":[""]
+},
+"food":{
+"itemfood":[""],
+"qfood":[""]
+} ,
+"status": "AVAILABLE",
+"table": "B",
+"time": ""
+},
+"C":{
+"drinks":{
+"itemdrink":[""],
+"qdrink":[""]
+},
+"food":{
+"itemfood":[""],
+"qfood":[""]
+} ,
+"status": "AVAILABLE",
+"table": "C",
+"time": ""
+},
+"D":{
+"drinks":{
+"itemdrink":[""],
+"qdrink":[""]
+},
+"food": {
+"itemfood":[""],
+"qfood":[""]
+} ,
+"status": "AVAILABLE",
+"table": "D",
+"time": ""
+},
+"E":{
+"drinks":{
+"itemdrink":[""],
+"qdrink":[""]
+},
+"food": {
+"itemfood":[""],
+"qfood":[""]
+} ,
+"status": "AVAILABLE",
+"table": "E",
+"time": ""
+},
+"F":{
+"drinks":{
+"itemdrink":[""],
+"qdrink":[""]
+},
+"food":{
+"itemfood":[""],
+"qfood":[""]
+} ,
+"status": "AVAILABLE",
+"table": "F",
+"time": ""
+}
+}
